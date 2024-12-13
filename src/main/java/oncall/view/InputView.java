8d7d9c8faa43a00d426495169445e5d7890c4211
goohong <goohong.chung@gmail.com>
2024-12-13 14:37:29 +0900

    feat(InputView): 월(숫자)과 시작 요일(일, 월, 화, 수, 목, 금, 토) 정보를 입력받는다.

--- src/main/java/oncall/view/InputView.java
+++ src/main/java/oncall/view/InputView.java
@@ -1,4 +1,12 @@
 package oncall.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    private static final String MONTH_AND_DAY_PROMPT = "비상 근무를 배정할 월과 시작 요일을 입력하세요> ";
+
+    public String askMonthAndDay() {
+        System.out.println(MONTH_AND_DAY_PROMPT);
+        return Console.readLine();
+    }
 }
