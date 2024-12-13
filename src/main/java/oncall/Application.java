5a27ff2f7abc856fa196ae6081e8a3b879cb2f12
goohong <goohong.chung@gmail.com>
2024-12-13 14:32:31 +0900

    feat(Application): 기본 MVC 패턴 구조 생성

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,7 +1,24 @@
 package oncall;
 
+import oncall.controller.OncallController;
+import oncall.view.InputView;
+import oncall.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new Application().run();
+    }
+
+    private void run() {
+        OncallController oncallController = initializeComponents();
+        oncallController.run();
+    }
+
+    private OncallController initializeComponents() {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        return new OncallController(inputView, outputView);
     }
 }
