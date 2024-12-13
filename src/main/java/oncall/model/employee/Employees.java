4dd4dc2f9271830ea4a84aeb074f71d623da50f5
goohong <goohong.chung@gmail.com>
2024-12-13 15:50:26 +0900

    feat(Employees): 근무자의 닉네임은 중복이 되어서는 안된다.

--- src/main/java/oncall/model/employee/Employees.java
+++ src/main/java/oncall/model/employee/Employees.java
@@ -14,6 +14,7 @@ public class Employees {
 
     public static Employees ofValue(List<String> nicknames) {
         checkDuplicate(nicknames);
+        checkCountInRange(nicknames);
 
         return new Employees(
                 nicknames.stream()
@@ -25,6 +26,12 @@ public class Employees {
     private static void checkDuplicate(List<String> nicknames) {
         if (nicknames.size() != new HashSet<>(nicknames).size()) {
             throw new IllegalArgumentException(ErrorCode.EMPLOYEE_NICKNAME_DUPLICATED.getMessage());
+        }
+    }
+
+    private static void checkCountInRange(List<String> nicknames) {
+        if (nicknames.size() < 5 || 35 < nicknames.size()) {
+            throw new IllegalArgumentException(ErrorCode.EMPLOYEE_COUNT_OUT_OF_RANGE.getMessage());
         }
     }
 }
